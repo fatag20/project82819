93cd59801515753ff1d8b7010c1037887c6cff8c
fatag20 <54539265+fatag20@users.noreply.github.com>
2019-10-02 08:55:32 -0500

    10219

---
+++ togglemenu.js
@@ -0,0 +1,8 @@
+  
+function toggleMenu() {
+    if (document.getElementById("main-nav").style.display == "none") {
+        document.getElementById("main-nav").style.display = "block";
+    } else {
+        document.getElementById("main-nav").style.display = "none";
+    }
+}
