594ad606c780f73347a52af8c594485b39725042
fatag20 <54539265+fatag20@users.noreply.github.com>
2019-10-23 09:01:15 -0500

    10/23/19

--- togglemenu.js
+++ togglemenu.js
@@ -1,5 +1,16 @@
-  
 function toggleMenu() {
+    if (document.getElementById("main-nav").style.height == "0px") {
+        var numChildren = document.getElementById("main-nav").childNodes.length;
+        var heightFactor = window.getComputedStyle(document.body).getPropertyValue('--height-factor');
+        document.getElementById("main-nav").style.height = ((numChildren) * heightFactor) + "em";
+        // document.getElementById("main-nav").style.height = "100vh";
+        document.getElementById("burger").classList.add("open");
+    } else {
+        document.getElementById("main-nav").style.height = "0px";
+        document.getElementById("burger").classList.remove("open");
+    }
+}
+/*function toggleMenu() {
     if (document.getElementById("main-nav").style.height == "0px") {
         var numChildren = document.getElementById("main-nav").childNodes.length;
         document.getElementById("main-nav").style.height = (numChildren * 1.5) + "em";
@@ -8,4 +19,4 @@ function toggleMenu() {
         document.getElementById("main-nav").style.height = "0px";
         document.getElementById("burger").classList.remove("open");
     }
-}
+}*/
